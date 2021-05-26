b6dcc2c0d6ceda8ec27bf977d9cdae1ecf49f8db
john alexander agudelo <johnaagudelo@gmail.com>
2021-05-26 14:42:58 -0500

    feat(Home): implement ItemListCrypto in ListFlat

--- apps/cryptocoin-app/src/app/home/home-screen.tsx
+++ apps/cryptocoin-app/src/app/home/home-screen.tsx
@@ -1,19 +1,25 @@
 import React from 'react';
 import { View, FlatList, StyleSheet, ActivityIndicator, Text } from 'react-native';
+import { useNavigation } from '@react-navigation/core';
 
 import { useRecoilState } from 'recoil';
 import { assetsAtom } from '@coincap/atoms'
 import { useFetchAssets } from '@coincap/hooks';
-
-import Color from '../res/colors';
+import { ItemListCrypto } from '@coincap/ui-mobile';
+import { colors } from '@coincap/utils' 
 
 export function HomeScreen() {
+  const navigation = useNavigation();
   const [assets, setAssets] = useRecoilState(assetsAtom);
-
-  console.log(assets);
 
   // custom hook inital component
   useFetchAssets('https://api.coincap.io/v2/assets?limit=20', setAssets);
+
+  const handlerPress = (idCryto: string) => {
+    navigation.navigate('CoinDetail', {
+      idCryto
+    });
+  }
 
   return (
     <View style={styles.container}>
@@ -27,7 +33,15 @@ export function HomeScreen() {
         <FlatList
           data={assets.list}
           renderItem={({ item }) => (
-            <Text>{item.name}</Text>
+            <ItemListCrypto
+              id={item.id}
+              name={item.name}
+              symbol={item.symbol}
+              priceUsd={item.priceUsd}
+              changePercent24Hr={item.changePercent24Hr}
+              key={item.id}
+              onPress={handlerPress}
+            />
           )}
         />
       )}
@@ -38,7 +52,7 @@ export function HomeScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: Color.charade,
+    backgroundColor: colors.charade,
     justifyContent: 'center',
   },
   titleText: {
