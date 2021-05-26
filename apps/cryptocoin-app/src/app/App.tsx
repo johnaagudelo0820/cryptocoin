e0adc122a76472c5e44b6bb30fc3db8e9a21565f
john alexander agudelo <johnaagudelo@gmail.com>
2021-05-26 08:59:04 -0500

    feat(Navigator): Implement tabs and react navigator

--- apps/cryptocoin-app/src/app/App.tsx
+++ apps/cryptocoin-app/src/app/App.tsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import { NavigationContainer } from '@react-navigation/native'; 
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import {
   SafeAreaView,
   StyleSheet,
@@ -10,126 +12,58 @@ import {
   TouchableOpacity,
 } from 'react-native';
 
-import {
+import Color from './res/colors';
+
+const Tabs = createBottomTabNavigator();
+
+/* import {
   Colors,
   DebugInstructions,
   ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
 // @ts-ignore
-import openURLInBrowser from 'react-native/Libraries/Core/Devtools/openURLInBrowser';
+import openURLInBrowser from 'react-native/Libraries/Core/Devtools/openURLInBrowser'; */
+
+const tabBarOptions = {
+  tintColor: "#fefefe",
+  style: {
+    backgroundColor: Color.blackPearl,
+  }
+};
 
 const App = () => {
   return (
-    <>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}
-        >
-          <View style={styles.header}>
-            <Image style={styles.logo} source={require('./logo.png')} />
-            <Text style={styles.heading} testID="heading">
-              Welcome to React Native
-            </Text>
-          </View>
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One</Text>
-              <Text style={styles.sectionDescription}>
-                Edit{' '}
-                <Text style={styles.highlight}>
-                  apps/cryptocoin-app/App.tsx
-                </Text>{' '}
-                to change this screen and then come back to see your edits.
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions /> Alternatively, press{' '}
-                <Text style={styles.highlight}>R</Text> in the bundler terminal
-                window.
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <TouchableOpacity
-                accessibilityRole="button"
-                onPress={() => openURLInBrowser('https://nx.dev')}
-              >
-                <Text style={styles.sectionDescription}>
-                  Visit <Text style={styles.link}>nx.dev</Text> for more info
-                  about Nx.
-                </Text>
-              </TouchableOpacity>
-            </View>
-          </View>
-        </ScrollView>
-      </SafeAreaView>
-    </>
+    <NavigationContainer>
+      <Tabs.Navigator
+        tabBarOptions={tabBarOptions}
+      >
+          <Tabs.Screen
+            name="Coins"
+            component={() => <Text>Coins</Text>}
+            options={{
+              tabBarIcon: ({ size, color }) => (
+                <Image
+                  style={{ tintColor: color, width: size, height: size }}
+                  source={require('./assets/bank.png')}
+                />
+              ),
+            }}
+          />
+          <Tabs.Screen
+            name="Favorite"
+            component={() => <Text>Setttings</Text>}
+            options={{
+              tabBarIcon: ({ size, color }) => (
+                <Image
+                  style={{ tintColor: color, width: size, height: size }}
+                  source={require('./assets/star.png')}
+                />
+              ),
+            }}
+          />
+      </Tabs.Navigator>
+    </NavigationContainer>
   );
 };
-
-const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
-  },
-  header: {
-    backgroundColor: '#143055',
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    paddingVertical: 24,
-  },
-  logo: {
-    width: 200,
-    height: 180,
-    resizeMode: 'contain',
-  },
-  heading: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.lighter,
-  },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
-  },
-  link: {
-    color: '#45bc98',
-  },
-});
 
 export default App;
