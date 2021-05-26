7219c869c9e0f7ffc7311ad67eff5a77f6195272
john alexander agudelo <johnaagudelo@gmail.com>
2021-05-26 09:05:12 -0500

    feat(Home): Create home view and real time

---
+++ apps/cryptocoin-web/src/view/home/home.tsx
@@ -0,0 +1,60 @@
+import React from 'react';
+import { useHistory } from "react-router-dom";
+import { List, Paper } from '@material-ui/core';
+
+import { useWebSocket, useFetchAssets } from '@coincap/hooks';
+import { ItemListCrypto } from '@coincap/ui-web';
+import { useRecoilState } from 'recoil';
+
+import { assetsAtom } from '../../recoil/atoms';
+
+export function Home() {
+  const history = useHistory();
+  const [assets, setAssets] = useRecoilState(assetsAtom);
+
+  const updateAssetCrypto = (e: any) => {
+    const updateCurrent = JSON.parse(e.data);
+    setAssets((oldAssets) => {
+      const assetsCoin = Object.assign({}, oldAssets.hashCoins);
+      Object.keys(updateCurrent).forEach(current => {
+        const crypto = Object.assign({}, { ...assetsCoin[current], priceUsd: updateCurrent[current] });
+        assetsCoin[current] = crypto;
+      })
+      return {
+        ...oldAssets,
+        hashCoins: assetsCoin,
+      }
+    });
+  };
+  useWebSocket(assets.assetParam, updateAssetCrypto);
+
+  const handlerOnClick = (id: string) => {
+    history.push(`/${id}`);
+  }
+
+  // custom hook inital component
+  useFetchAssets('https://api.coincap.io/v2/assets?limit=20', setAssets);
+
+  return (
+    <Paper>
+      <List>
+        {Object.keys(assets.hashCoins).map(idAsset => {
+          const asset = assets.hashCoins[idAsset];
+          return (
+            <ItemListCrypto
+              id={asset.id}
+              name={asset.name}
+              symbol={asset.symbol}
+              priceUsd={asset.priceUsd}
+              changePercent24Hr={asset.changePercent24Hr}
+              key={asset.id}
+              handlerOnClick={handlerOnClick}
+            />
+          )
+        })}
+      </List>
+    </Paper>
+  );
+}
+
+export default Home;
