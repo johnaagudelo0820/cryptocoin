4725077d0e401583dbbe4e453f21d33305a4f9f8
john alexander agudelo <johnaagudelo@gmail.com>
2021-05-26 11:37:28 -0500

    feat(Interfaces): add interfaces Asset API

--- libs/recoil/interfaces/src/lib/recoil-interfaces.ts
+++ libs/recoil/interfaces/src/lib/recoil-interfaces.ts
@@ -3,9 +3,25 @@ export type AssetHistory = {
   time: string,
 }
 
+export interface Asset {
+  changePercent24Hr: string,
+  explorer: string,
+  id: string,
+  marketCapUsd: string,
+  maxSupply: string,
+  name: string,
+  priceUsd: string,
+  rank: string,
+  supply: string,
+  symbol: string,
+  volumeUsd24Hr: string,
+  vwap24Hr: string,
+}
+
 export interface AssetAtom {
   assetParam: string;
   hashCoins: any;
+  list: Array<Asset>;
   loading: boolean;
 };
 
