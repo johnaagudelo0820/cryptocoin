da286cd25c4e723eda7ae41e75000f596a4b36ea
john alexander agudelo <johnaagudelo@gmail.com>
2021-05-26 22:10:13 -0500

    feat(Header): implement header in layout

--- apps/cryptocoin-web/src/app/template/template.tsx
+++ apps/cryptocoin-web/src/app/template/template.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import { useRecoilState } from 'recoil';
-import { Container, Grid, Switch } from '@material-ui/core'
+import { Container, Grid, Box } from '@material-ui/core'
 
 import { darkModeAtom } from '@coincap/atoms';
+import { Header } from '@coincap/ui-web';
 
 /* eslint-disable-next-line */
 export interface TemplateProps {
@@ -12,17 +13,21 @@ export interface TemplateProps {
 export function Template(props: TemplateProps) {
   const [darkMode, setDarkMode] = useRecoilState(darkModeAtom);
   return (
-    <Container>
-      <Switch checked={darkMode} onChange={() => setDarkMode(!darkMode)} />
-      <Grid
-        container
-      >
-        <Grid item xs={12}>
-          <h1>header</h1>
-          {props.children}
-        </Grid>
-      </Grid>
-    </Container>
+    <>
+      <Header
+        darkMode={darkMode}
+        onChange={() => setDarkMode(!darkMode)}
+      />
+      <Box marginTop={4}>
+        <Container>
+          <Grid container>
+            <Grid item xs={12}>
+              {props.children}
+            </Grid>
+          </Grid>
+        </Container>
+      </Box>
+    </>
   );
 }
 
