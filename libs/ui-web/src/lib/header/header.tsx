02e896ded4341831ebcb3935026b3d62be2b909d
john alexander agudelo <johnaagudelo@gmail.com>
2021-05-26 22:13:14 -0500

    feat(ui-web): add header component

---
+++ libs/ui-web/src/lib/header/header.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Switch from '@material-ui/core/Switch';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+}));
+
+/* eslint-disable-next-line */
+export interface HeaderProps {
+  onChange: () => void,
+  darkMode: boolean,
+}
+
+export function Header({ onChange, darkMode }: HeaderProps) {
+  const classes = useStyles();
+  return (
+    <div className={classes.root}>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6" className={classes.title}>
+            Coincap
+          </Typography>
+          <Switch checked={darkMode} onChange={onChange} />
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
+}
+
+export default Header;
