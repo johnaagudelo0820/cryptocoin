d617fe3a56baeeeed2acfc4c8ffdb13a7d8fdcd7
john alexander agudelo <johnaagudelo@gmail.com>
2021-05-26 22:11:21 -0500

    feat(Detail): update interface and styles

--- apps/cryptocoin-web/src/view/detail/detail.tsx
+++ apps/cryptocoin-web/src/view/detail/detail.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import { useParams } from "react-router-dom";
-import { useRecoilState } from 'recoil';
-import Chart from 'kaktana-react-lightweight-charts';
-import { Box } from '@material-ui/core';
+import { useRecoilState, useRecoilValue } from 'recoil';
+import { Grid, Paper, Divider } from '@material-ui/core';
 
-import { assetdetailAtom } from '@coincap/atoms';
+import { assetdetailAtom, darkModeAtom } from '@coincap/atoms';
 import { useFetchDetailAsset, useSuscriptionPriceHistory } from '@coincap/hooks';
 import { formatterNumberWithDecimals, cloneObject } from '@coincap/utils';
+import { ChartLine, HeaderDetail } from '@coincap/ui-web';
 
 export interface paramRoute {
   idCrypto: string,
@@ -15,30 +15,47 @@ export interface paramRoute {
 export function Detail() {
   const { idCrypto }: paramRoute = useParams();
   const [asset, setAsset] = useRecoilState(assetdetailAtom);
+  const darkMode = useRecoilValue(darkModeAtom);
   useFetchDetailAsset(idCrypto, setAsset);
   useSuscriptionPriceHistory(idCrypto, setAsset);
+  const getImageArrow = (upCurrency: boolean) => {
+    if (upCurrency) {
+      return '../../assets/arrow_up.png';
+    }
+    return '../../assets/arrow_down.png';
+  }
 
-  const lineSeries = [{
-    data: cloneObject(asset.history),
-  }];
-  const { priceUsd } = asset.data;
+  const { priceUsd, name, symbol, isUp } = asset.data;
   return (
-    <Box display="flex" >
-      <Box>{formatterNumberWithDecimals(priceUsd, 2)}</Box>
-      <Chart
-        options={{
-          localization: {
-            priceFormatter: function(price: string) {
-              return `$${formatterNumberWithDecimals(price, 2)}`;
-            }
-          }
-        }}
-        lineSeries={lineSeries}
-        autoWidth
-        height={600}
-        darkTheme={true}
-      />
-    </Box>
+    <Grid container spacing={2}>
+      <Grid item xs={12}>
+        <HeaderDetail
+          name={name}
+          priceUsd={priceUsd}
+          symbol={symbol}
+          imageUp={<img alt="indicator" src={getImageArrow(isUp)} width={40} height={40} />}
+        />
+      </Grid>
+      <Grid item xs={12}>
+        <Divider />
+      </Grid>
+      <Grid item xs={12}>
+        <Paper>
+          <ChartLine
+            options={{
+              localization: {
+                priceFormatter: function(price: string) {
+                  return `$${formatterNumberWithDecimals(price, 2)}`;
+                }
+              }
+            }}
+            data={cloneObject(asset.history)}
+            height={400}
+            darkTheme={darkMode}
+          />
+        </Paper>
+      </Grid>
+    </Grid>
   );
 }
 
