bb9687cae822278530048453cdfe064e5a2df6f2
john alexander agudelo <johnaagudelo@gmail.com>
2021-05-28 18:10:57 -0500

    test(Web): test render loyaut template

--- apps/cryptocoin-web/src/app/template/template.spec.tsx
+++ apps/cryptocoin-web/src/app/template/template.spec.tsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 
+import { RecoilRoot } from 'recoil';
+
 import Template from './template';
 
 describe('Template', () => {
   it('should render successfully', () => {
-    const { baseElement } = render(<Template />);
+    const { baseElement } = render(
+      <RecoilRoot>
+        <Template>children</Template>
+      </RecoilRoot>
+    );
     expect(baseElement).toBeTruthy();
   });
 });
