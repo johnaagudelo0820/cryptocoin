020e28760862170492a4f554347c197c6a4910b4
john alexander agudelo <johnaagudelo@gmail.com>
2021-05-28 18:07:55 -0500

    style(App): Implement theme in screen settings and switch

--- apps/cryptocoin-app/src/app/settings/settings-screen.tsx
+++ apps/cryptocoin-app/src/app/settings/settings-screen.tsx
@@ -1,18 +1,21 @@
 import React from 'react';
 import { useRecoilState } from 'recoil';
 import { Text, Switch, StyleSheet, SafeAreaView } from 'react-native';
+import { useTheme } from '@react-navigation/native';
 
 import { darkModeAtom } from '@coincap/atoms';
+import { themePalette } from '@coincap/utils';
 
 export function SettingsScreen() {
   const [darkMode, setDarkMode] = useRecoilState(darkModeAtom);
+  const { colors } = useTheme();
   return (
     <SafeAreaView style={styles.container}>
-      <Text>Dark mode</Text>
+      <Text style={{ color: colors.text }}>Dark mode</Text>
       <Switch
-        trackColor={{ false: "#767577", true: "#81b0ff" }}
-        thumbColor={darkMode ? "#f5dd4b" : "#f4f3f4"}
-        ios_backgroundColor="#3e3e3e"
+        trackColor={{ false: colors.border, true: themePalette.white }}
+        thumbColor={colors.card}
+        ios_backgroundColor={colors.card}
         onValueChange={() => setDarkMode(!darkMode)}
         value={darkMode}
       />
