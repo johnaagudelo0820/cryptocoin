966120b220c6ef1b3e71400acb7f2723ee86ff58
john alexander agudelo <johnaagudelo@gmail.com>
2021-05-28 18:12:01 -0500

    test(Web): test render home view

--- apps/cryptocoin-web/src/view/home/home.spec.tsx
+++ apps/cryptocoin-web/src/view/home/home.spec.tsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import { render } from '@testing-library/react';
+import { RecoilRoot } from 'recoil';
 
 import Home from './home';
 
 describe('Home', () => {
   it('should render successfully', () => {
-    const { baseElement } = render(<Home />);
+    const { baseElement } = render(
+      <RecoilRoot>
+        <Home />
+      </RecoilRoot>
+    );
     expect(baseElement).toBeTruthy();
   });
 });
