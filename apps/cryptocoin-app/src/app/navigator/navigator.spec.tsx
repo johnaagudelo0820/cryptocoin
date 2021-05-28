a6188898be00fec42883ba1eee8f4a149b8ff6f5
john alexander agudelo <johnaagudelo@gmail.com>
2021-05-28 18:06:48 -0500

    test(App): navigator render with prop theme

--- apps/cryptocoin-app/src/app/navigator/navigator.spec.tsx
+++ apps/cryptocoin-app/src/app/navigator/navigator.spec.tsx
@@ -1,11 +1,23 @@
 import React from 'react';
 import { render } from '@testing-library/react';
+import { themePalette } from '@coincap/utils';
 
 import Navigator from './navigator';
 
+const themeLight = {
+  colors: {
+    primary: themePalette.primary.main,
+    background: themePalette.white,
+    card:  themePalette.primary.dark,
+    text: themePalette.secondary.dark,
+    border: themePalette.primary.light,
+    notification: themePalette.primary.main,
+  },
+};
+
 describe('Navigator', () => {
   it('should render successfully', () => {
-    const { baseElement } = render(<Navigator />);
+    const { baseElement } = render(<Navigator theme={themeLight} />);
     expect(baseElement).toBeTruthy();
   });
 });
