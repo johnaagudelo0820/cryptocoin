2a599ec9363a2c8b9f7433de4e9f9520e1cbd2e9
john alexander agudelo <johnaagudelo@gmail.com>
2021-05-28 18:01:13 -0500

    test(App): test render Home list

--- apps/cryptocoin-app/src/app/home/home-list.spec.tsx
+++ apps/cryptocoin-app/src/app/home/home-list.spec.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import { render } from '@testing-library/react';
-
 import HomeList from './home-list';
+
+import { MockAssets } from '@coincap/utils'; 
 
 describe('HomeList', () => {
   it('should render successfully', () => {
-    const { baseElement } = render(<HomeList />);
+    const { baseElement } = render(<HomeList assets={MockAssets}/>);
     expect(baseElement).toBeTruthy();
   });
 });
