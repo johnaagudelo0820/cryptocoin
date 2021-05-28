4ac7e04cd1973717ae8e2582ac3760c324cd8bd5
john alexander agudelo <johnaagudelo@gmail.com>
2021-05-28 18:03:53 -0500

    feat(Theme): implement theme navigator

--- apps/cryptocoin-app/src/app/home/home-screen.tsx
+++ apps/cryptocoin-app/src/app/home/home-screen.tsx
@@ -1,26 +1,27 @@
 import React from 'react';
 import { View, StyleSheet, ActivityIndicator } from 'react-native';
+import { useTheme } from '@react-navigation/native';
 
 import { useRecoilState } from 'recoil';
 import { assetsAtom } from '@coincap/atoms'
 import { useFetchAssets } from '@coincap/hooks';
-import { colors } from '@coincap/utils';
+import { config } from '@coincap/utils';
 
 import HomeList from './home-list';
 
 export function HomeScreen() {
   const [assets, setAssets] = useRecoilState(assetsAtom);
+  const { colors } = useTheme();
 
   // custom hook inital component
-  useFetchAssets('https://api.coincap.io/v2/assets?limit=20', setAssets);
+  useFetchAssets(`${config.API_BASE_URL}?limit=20`, setAssets);
 
   return (
     <View style={styles.container}>
       {assets.loading ? (
         <ActivityIndicator
-          color="#fff"
+          color={colors.card}
           size="large"
-          style={styles.loader}
         />
       ) : (
         <HomeList assets={assets} />
@@ -32,26 +33,8 @@ export function HomeScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: colors.charade,
     justifyContent: 'center',
   },
-  titleText: {
-    color: 'white',
-    textAlign: 'center',
-  },
-  btn: {
-    padding: 8,
-    backgroundColor: 'blue',
-    borderRadius: 8,
-    margin: 16,
-  },
-  btnText: {
-    color: 'white',
-    textAlign: 'center'
-  },
-  loader: {
-    marginTop: 60,
-  }
 });
 
 export default HomeScreen;
