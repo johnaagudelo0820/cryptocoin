c5dc7702d833444065e9238c8dbcaa93f9f83f0a
john alexander agudelo <johnaagudelo@gmail.com>
2021-05-30 18:37:11 -0500

    test(Cypress): add test e2e

--- apps/cryptocoin-web-e2e/src/integration/app.spec.ts
+++ apps/cryptocoin-web-e2e/src/integration/app.spec.ts
@@ -1,10 +1,29 @@
-import { getGreeting } from '../support/app.po';
-
 describe('cryptocoin-web', () => {
   beforeEach(() => cy.visit('/'));
 
-  it('should display welcome message', () => {
+  it('should display title message', () => {
     // Function helper example, see `../support/app.po.ts` file
-    getGreeting().contains('Coinapp');
+    cy.get('.MuiToolbar-root h6').should('contain', 'Coincap');
+  });
+
+  it('should load home wiew', () => {
+    cy.url().should('include', '/');
+  });
+
+  it('should navigate to Bitcoin detail', () => {
+    cy.get(':nth-child(1) > .MuiButtonBase-root > .MuiListItemText-root > .MuiListItemText-primary').click();
+    cy.url().should('include', '/bitcoin');
+    cy.get('.MuiTypography-h4').should('contain', 'Bitcoin');
+  });
+
+  it('should return home', () => {
+    cy.get('a').click();
+    cy.url().should('include', '/')
+  });
+
+  it('should navigate to Ethereum detail', () => {
+    cy.get(':nth-child(2) > .MuiButtonBase-root > .MuiListItemText-root > .MuiListItemText-primary').click();
+    cy.url().should('include', '/ethereum')
+    cy.get('.MuiTypography-h4').should('contain', 'Ethereum');
   });
 });
--- libs/ui-web/src/lib/header/header.tsx
+++ libs/ui-web/src/lib/header/header.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import { makeStyles } from '@material-ui/core/styles';
+import { Link } from 'react-router-dom';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
@@ -31,8 +32,10 @@ export function Header({ onChange, darkMode, title }: HeaderProps) {
     <div className={classes.root}>
       <AppBar position="static">
         <Toolbar>
-          <Typography variant="h6" className={classes.title}>
-            {title}
+        <Typography variant="h6" className={classes.title}>
+            <Link to="/" style={{ color: 'white', textDecoration: 'none'}}>
+              {title}
+            </Link>
           </Typography>
           <Switch checked={darkMode} onChange={onChange} />
         </Toolbar>
